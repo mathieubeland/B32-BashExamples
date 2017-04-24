3a98c8546f22d192fe3297ed4b5f9dd1080d7c12
Mathieu Béland <mathieubeland00@gmail.com>
2017-04-24 17:34:59 -0400

    comment

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+echo "salut"
+#exit Le fichier s'arrête
+echo "toi"
+
+# -n = Prévenir un retour de ligne automatique
+echo -n "Vive" #On ne fait pas de nouvelle ligne
+echo " Linux"
+
+#Pemet de mettre des espace
+echo
+echo -e "\n"
+
+# -e = Accepter les méta-caractères (\n,\t, ...)
+echo -e "1\n2\n3" #Permet d'interpréter les \n
+
+# $ = lorsque l'in veut la valeur de la variable (lecture)
+# sinon, on affecte dans la variable (
+read -p^"Quel âge avez-vous" age 
+echo $age3 "ans! Vous paraissex jeune!"
