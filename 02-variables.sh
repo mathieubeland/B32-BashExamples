85f24957ba2146571330e63beaa28283b5d7e361
Mathieu Béland <mathieubeland00@gmail.com>
2017-04-24 17:54:58 -0400

    Added conditions

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+#Ne pas mettre d'espace!!!
+prenom=John
+echo $prenom
+declare -i age=35 #Un int
+declare -r age2=33 #Devient en lecture seule
+
+#On met des guillemets lorsqu'il y a des espaces
+cours = "Cours de Linux"
+
+#Ces variables prendront la valeur des paramètres donées en ligne de commande.
+echo Le programme $0 a pris le paramètre $1
--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+note=60
+if test $note -lt 60
+then 
+	echo "you failed!"
+elif test $note -eq 60
+then
+	echo "ouf! c'est juste!"
+else
+	echo "Alright"
+fi
+#fi ferme le if
 
+read -p "Entrez une lecture:" lettre
 
-
-
+case $lettre in
+	[[:lower]])
+		echo "lettre en minuscule"
+		;; #pour dire ou ça finit
+	*)
+		echo "Majuscule"
+		;;
+esac #fermer le case, case à l'envers
 
 
 
