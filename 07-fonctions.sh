a05778ecc6734e77fcd24169a1d90a7abafd9c15
Mathieu Béland <mathieubeland00@gmail.com>
2017-04-26 15:02:39 -0400

    Ajour des focntions

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+#C'est un langage non typé...Pas besoin de mettre de type.
+maFonction() {
+	echo "salut $1"
+}
+
+#Appel des fonctions, pas besoin de mettre de parenthèses.
+maFonction MarcO
+maFonction JS
+maFonction JU
