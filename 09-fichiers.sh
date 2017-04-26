2dc240df7f1b4776a087831ec9305458bf60005b
Mathieu Béland <mathieubeland00@gmail.com>
2017-04-26 15:16:25 -0400

    Test sur les fichiers

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# -e veut dire exist, donc on veut savoir si le fichier existe
+# $# = Nombre de paramètre passés au programme
+# -ge = greater or equal
+if test $# -ge 1 #Si le nom est plus grand que 1
+then
+	if test -e $1
+	then
+		echo "Il existe"
+	fi
+fi
