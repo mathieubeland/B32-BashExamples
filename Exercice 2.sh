9174a13f37e92b2ff0453d92bfc67ae23891a12d
Mathieu Béland <mathieubeland00@gmail.com>
2017-04-26 16:16:21 -0400

    Menu

--- Exercice 2.sh
+++ Exercice 2.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+        clear
+        echo "Choississez une option"
+        echo "d. Dictionnaire"
+        echo "q. Quitter"
+        read -p "Quelle option voulez-vous: " choix
+
+        if test $choix = "d"
+        then
+		clear
+                ./Exercice\ 1.sh
+		sleep 3
+        fi
+
+
+while test $choix != "q"
+do
+	clear
+	echo "Choississez une option"
+        echo "d. Dictionnaire"
+        echo "q. Quitter"
+        read -p "Quelle option voulez-vous: " choix
+
+        if test $choix = "d"
+        then	
+		clear
+                ./Exercice\ 1.sh
+		sleep 3s
+        fi
+done
