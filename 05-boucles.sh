1fc7e67646dcc0f41db835a6bc5591bc475fa812
Mathieu Béland <mathieubeland00@gmail.com>
2017-04-26 14:56:25 -0400

    3 sortes de boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+#Les espaces sont importants
+#C'est important de mettre i++ et non ++i.
+for ((i=0; i<5; i++))
+do
+	echo $i
+done
+resultat=o
+
+while test $resultat = "o"
+do
+	read -p "Entrez un lettre : " resultat
+done
+
+maListe=`ls`
+
+#Affiche tous les éléments que ls donnerait à 
+#l'endroit où on exécute le fichier
+
+for element in $maListe
+do
+	echo $element
+done
