de44964d0c2e80084a109b2d8a826078e86dec20
Mathieu Béland <mathieubeland00@gmail.com>
2017-04-26 15:11:05 -0400

    On peut faire des exp. régulières

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+#Même chose que read -p "Lettre: " lettre
+echo -n "Lettre:"
+read lettres
+
+#~ = regexp!
+#Il faut que ça commence par au moins
+if [[ $lettres =~ ^[0-9]+$ ]]
+then 
+	echo "C'est un nombre"
+fi
+
+if [[ $lettres =~ ^[a-zA-Z]+$ ]]
+then
+        echo "C'est un mot"
+fi
+
